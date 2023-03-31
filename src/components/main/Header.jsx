c0fe5c35fa3d3f8b6f36758c2913e5259738a428
Ostap Chichiginarov <ostap00034@gmail.com>
2023-04-01 01:58:37 +0900

    add test responsive functional

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,48 +1,75 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Button from "../ui/Button";
+import HiddenMenu from "./HiddenMenu";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <header
-      id="main"
-      className="flex h-[128px] w-full flex-row items-center justify-between"
-    >
-      <Link href="/" scroll={false}>
-        <div className="flex flex-row items-center justify-center gap-[24px]">
-          <Image src="/logo.svg" priority width="56" height="56" alt="logo" />
-          <h2 className="font-gilroy-bold text-[36px] not-italic  leading-[45px] text-white">
-            MiCROFARM
-          </h2>
-        </div>
-      </Link>
+    <>
+      <HiddenMenu open={open} setOpen={setOpen} />
+      <header
+        id="main"
+        className="flex h-[128px] w-full flex-row items-center justify-between"
+      >
+        <Link href="/" scroll={false}>
+          <div className="flex flex-row items-center justify-center gap-[24px]">
+            <Image src="/logo.svg" priority width="56" height="56" alt="logo" />
+            <h2 className="font-gilroy-bold text-[36px] not-italic  leading-[45px] text-white">
+              MiCROFARM
+            </h2>
+          </div>
+        </Link>
 
-      <div className="flex flex-row items-center gap-[48px] font-gilroy-bold text-[18px] leading-[22.28px] text-white">
-        <Link
-          href="/#about_us"
-          className="duration-300 hover:text-green"
-          scroll={false}
+        <div className=" hidden flex-row items-center gap-[48px] font-gilroy-bold text-[18px] leading-[22.28px] text-white xl:flex">
+          <Link
+            href="/#about_us"
+            className="duration-300 hover:text-green"
+            scroll={false}
+          >
+            О нас
+          </Link>
+          <Link
+            href="/#advantages"
+            className="duration-300 hover:text-green"
+            scroll={false}
+          >
+            Преимущества
+          </Link>
+          <Link
+            href="/#technology"
+            className="duration-300 hover:text-green"
+            scroll={false}
+          >
+            Технология
+          </Link>
+          <Button text="Связаться" />
+        </div>
+        <div
+          className="z-50 flex h-[60px] w-[80px] flex-col items-center justify-around xl:hidden"
+          onClick={() => {
+            setOpen(!open);
+          }}
         >
-          О нас
-        </Link>
-        <Link
-          href="/#advantages"
-          className="duration-300 hover:text-green"
-          scroll={false}
-        >
-          Преимущества
-        </Link>
-        <Link
-          href="/#technology"
-          className="duration-300 hover:text-green"
-          scroll={false}
-        >
-          Технология
-        </Link>
-        <Button text="Связаться" />
-      </div>
-    </header>
+          <span
+            className={`${
+              open ? "translate-y-[20px] rotate-45" : ""
+            } block h-[8px] w-[60px] transform cursor-pointer rounded-[3px] bg-green transition duration-300 ease-in-out`}
+          ></span>
+          <span
+            className={`block h-[8px] ${
+              open ? "w-0" : "w-[60px]"
+            } cursor-pointer rounded-[3px] bg-green duration-300 ease-in-out`}
+          ></span>
+          <span
+            className={`${
+              open ? "-translate-y-[20px] -rotate-45" : ""
+            } block h-[8px] w-[60px] transform cursor-pointer  rounded-[3px] bg-green transition duration-300 ease-in-out`}
+          ></span>
+        </div>
+      </header>
+    </>
   );
 };
 
---
+++ src/components/main/HiddenMenu.jsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import React from "react";
+import Button from "../ui/Button";
+
+const HiddenMenu = ({ open, setOpen }) => {
+  return (
+    <div
+      className={`absolute left-0 top-0 z-50 h-screen w-screen transform bg-dark ${
+        open ? "-translate-y-0" : "-translate-y-full"
+      } filter transition-transform duration-300 ease-in-out  `}
+    >
+      <div className="flex h-screen flex-col items-center justify-center gap-[48px] py-[48px] font-gilroy-bold text-[18px] leading-[22.28px] text-white">
+        <Link
+          onClick={() => {
+            setOpen(!open);
+          }}
+          href="/#about_us"
+          className="duration-300 hover:text-green"
+          scroll={false}
+        >
+          О нас
+        </Link>
+        <Link
+          onClick={() => {
+            setOpen(!open);
+          }}
+          href="/#advantages"
+          className="duration-300 hover:text-green"
+          scroll={false}
+        >
+          Преимущества
+        </Link>
+        <Link
+          onClick={() => {
+            setOpen(!open);
+          }}
+          href="/#technology"
+          className="duration-300 hover:text-green"
+          scroll={false}
+        >
+          Технология
+        </Link>
+        <Button text="Связаться" />
+      </div>
+    </div>
+  );
+};
+
+export default HiddenMenu;

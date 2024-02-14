5bcf803b77f9a870973af96d1827adc737bcc906
Ostap Chichiginarov <ostap00034@gmail.com>
2024-02-14 20:46:56 +0900

    test get form added

--- src/components/main/FeedBack.jsx
+++ src/components/main/FeedBack.jsx
@@ -32,7 +32,11 @@ const FeedBack = () => {
         Связаться с нами
       </h2>
       <div className="flex w-full flex-col items-center justify-between gap-[24px] xl:flex-row">
-        <form className="flex flex-col items-center justify-center gap-[24px]">
+        <form
+          action="https://getform.io/f/0dNVwDdL"
+          method="POST"
+          className="flex flex-col items-center justify-center gap-[24px]"
+        >
           <input
             value={data.fio}
             onChange={(e) =>
@@ -42,6 +46,7 @@ const FeedBack = () => {
               }))
             }
             type="text"
+            name="fio"
             placeholder="Ваше ФИО"
             className="h-[60px] w-[80vw] min-w-[40vw] rounded-[12px] py-[20px] px-[16px] font-gilroy-medium text-[16px] leading-[19.41px] xl:w-auto"
           />
@@ -54,6 +59,7 @@ const FeedBack = () => {
               }))
             }
             type="text"
+            name="phoneNumber"
             placeholder="Номер телефона"
             className="h-[60px] w-[80vw] min-w-[40vw] rounded-[12px] py-[20px] px-[16px] font-gilroy-medium text-[16px] leading-[19.41px] xl:w-auto"
           />
@@ -65,10 +71,12 @@ const FeedBack = () => {
                 description: e.target.value,
               }))
             }
+            name="description"
             placeholder="Кратко опишите ваш проект"
             className="h-[176px] w-[80vw] min-w-[40vw] rounded-[12px] py-[20px] px-[16px] font-gilroy-medium text-[16px] leading-[19.41px] xl:w-auto"
           />
-          <Button onClick={(e) => submit(e)} type="submit" text="Отправить" />
+          <Button type="submit" />
+          {/* <Button type="submit" onClick={(e) => submit(e)} text="Отправить" /> */}
         </form>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d935.5562972699518!2d129.7506451690147!3d62.032420347358766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5bf64bc95254bafd%3A0x9efb00617c6f4273!2zSVQt0J_QsNGA0Log0K_QutGD0YLRgdC6!5e0!3m2!1sen!2sru!4v1679254260481!5m2!1sen!2sru"
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-const Button = ({ text, ...rest }) => {
+const Button = ({ text, type = "button", ...rest }) => {
   return (
     <button
+      type={type}
       {...rest}
       className="h-[52px] max-w-[200px] rounded-[26px] bg-green px-[24px] py-[16px] font-gilroy-bold text-[18px] uppercase leading-[22.28px] text-white duration-300 hover:bg-[#0C6B2F]"
     >
